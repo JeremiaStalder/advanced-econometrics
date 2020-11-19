12c6a1673032f1ff4bae815d6ec7daf93f9f7019
LMGV <gorkunvoevoda@mail.ru>
2020-11-19 18:02:30 +0100

    .

--- code/non_para_M.R
+++ code/non_para_M.R
@@ -1,49 +1,83 @@
 library(tidyverse)
 library(np)
 library(bbemkr)
+library(dplyr)
+library(ks)
 
 setwd("C:/Users/lmgv/iCloudDrive/SG MiQEF/3 semester/Advanced Econometric Methods/Project/advanced-econometrics")
 load("./output/mydata_transform.Rdata")
 load("./output/variable_sets_descriptives.Rdata")
 load("./output/variable_sets_modelling.Rdata")
 
+source("./code/non_para_M_functions.R")
 
-npdata <- mydata_transform
-Y <- npdata$tw #numeric vector with outcome variable
-Ti <- npdata$e401 #Treatment 
-indep_vars <- variable_sets_modelling[["independent_vars_selection"]]
-squared_vars <- c("age_sq","age_cub","inc_sq","inc_cub")
 
-X <- select(npdata, all_of(indep_vars))
-#X <- select(X, -c("e401"))#no treatment
-X <- select(X, -squared_vars)#dont need squared variables
+npdata = mydata_transform
+Y = npdata$tw_adjust_std #numeric vector with outcome variable
+Ti = npdata$e401_std #Treatment 
+indep_vars = variable_sets_modelling[["independent_vars_selection_std"]]
+squared_vars = c("age_sq","age_cub","inc_sq","inc_cub")
 
-Y.0 = npdata[npdata[, "e401"] == 0,]$tw
-Y.1 = npdata[npdata[, "e401"] == 1,]$tw
+X <- dplyr::select(npdata, all_of(indep_vars))
+#X <- select(X, -c("e401"))
+#X <- dplyr::select(X, -squared_vars) #dont need squared variables
+
+
+## divide the data into two samples - where treatment equal to 0 and to 1
+Y.0 = npdata[npdata[, "e401_std"] == min(X[, "e401_std"]),]$tw_adjust_std
+Y.1 = npdata[npdata[, "e401_std"] == max(X[, "e401_std"]),]$tw_adjust_std
+
+X.0 = X[X[, "e401_std"] == min(X[, "e401_std"]),]
+X.1 = X[X[, "e401_std"] == max(X[, "e401_std"]),]
 
 #### Estimate the bandwidth ####
+
+# вопрос - нужно ли делать кернел на двух сабсэмплах по-отдельности, или на всем датасете вместе?
+
+## perform PCA first to reduce the number of features
+PC.0 = pcatr(X.0)
+PC.1 = pcatr(X.1)
+
+## use function npregbw to estimate the optimal bandwidth matrix - using three principal components
+## this function takes time to run, so it is commented. The results are loaded from saved RData files
+
+#bw.0 <- npregbw(xdat = PC.0[, 1:3], ydat = Y.0)
+#save(bw.0, file = "./output/non_para_M_bw_0.RData")
+
+#bw.1 <- npregbw(xdat = PC.1[, 1:3], ydat = Y.1)
+#save(bw.1, file = "./output/non_para_M_bw_1.RData")
+
+load("./output/non_para_M_bw_0.RData")
+load("./output/non_para_M_bw_1.RData")
+
+H.0 = Hlscv(PC.0[, 1:3])
+H.1 = Hlscv(PC.1[, 1:3])
+
+#### Kernel Estimation ####
+
+kd.0 = kde(PC.0[, 1:3], H = H.0)
+
+
+
+
+
+
 # сначала сделаю по всему датасету, чтобы посмотреть как оно работает. но потом, наверное, надо поделить на 
 # два датасета, где x=0 и x=1 и прогнать на этих двух частях по-отдельности
 # сначала посмотрю, со сколькими факторами норм по времени
-X1 = select(X, c("e401", "age", "inc"))
+X1 = dplyr:: select(X, c("e401", "age", "inc"))
 bw <- npregbw(xdat = X1, ydat = Y)
 save(bw, file = "./output/non_para_M_bw.RData")
 #dd <- npudens(bws = bw$bw, data = X1)
 
-# теперь на всём датасете (мб отдельно на двух подсэмплах?) - хрень
-bw.0 <- npregbw(xdat = Xe0, ydat = Y.0, nmulti = 3)
-save(bw, file = "./output/non_para_M_bw_Xe0.RData")
-
-bw.1 <- npregbw(xdat = Xe1, ydat = Y.1, nmulti = 3)
-save(bw, file = "./output/non_para_M_bw_Xe1.RData")
 
 ### теперь попробую сделать PCA до одной переменной
 ## first divide into two samples - where treatment equal to 0 and to 1
 Xe0 = X[X[, "e401"] == 0,]
 Xe1 = X[X[, "e401"] == 1,]
 
-Xe0 = select(Xe0, -c("e401"))
-Xe1 = select(Xe1, -c("e401"))
+Xe0 = dplyr::select(Xe0, -c("e401"))
+Xe1 = dplyr::select(Xe1, -c("e401"))
 
 ## PCA
 mean.0 = as.vector(colMeans(Xe0))
@@ -69,7 +103,7 @@ ym.1   = y.1[, 1]
 bw.cv.0 = npudensbw(ym.0, bwmethod="cv.ls")	# least squares cv for bw
 bw.cv.1 = npudensbw(ym.1, bwmethod="cv.ls")	# least squares cv for bw
 
-#### Kernel Estimation ####
+
 ## Nadaraya-Watson
 nwk.0 = NadarayaWatsonkernel(ym.0, Y.0, h = bw.cv.0$bw, gridpoint = seq(-3, 3, length.out = 100))
 nwk.1 = NadarayaWatsonkernel(ym.1, Y.1, h = bw.cv.1$bw, gridpoint = seq(-3, 3, length.out = 100))
@@ -77,3 +111,22 @@ nwk.1 = NadarayaWatsonkernel(ym.1, Y.1, h = bw.cv.1$bw, gridpoint = seq(-3, 3, l
 
 # использовать стандартизированные данные для bw и kernel
 # check if I can get standard errors for the estimator
+
+#### Scatter plot for PCs ####
+#mydata_transform_scatter = read.table("./output/mydata_transform_scatter.csv", sep = ",", header = TRUE)
+
+#X.scatter <- dplyr::select(mydata_transform_scatter, -c("tw_adjust_original"))
+#PC.scatter = pcatr(X.scatter)
+
+#PC.scatter["tw"] <- mydata_transform_scatter["tw_adjust_original"]
+
+#ggplot(PC.scatter, aes(x = V1, y = V2, color = tw)) + geom_point() + 
+#  scale_color_gradientn(colours = rainbow(5)) + 
+#  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+#        panel.grid.major = element_line(colour = "lightgrey"),
+#        panel.grid.minor = element_line(colour = "lightgrey")) +
+#  xlab("PC 1") +
+#  ylab("PC 2") +
+#  ggtitle("First two principal components")
+
+#ggsave("./output/descriptives/paper/first_two_PCs.png")
